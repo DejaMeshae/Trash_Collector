8304593dc8300156b7c8c1e661d511d5d516c498
DejaMeshae <daemeshae@yahoo.com>
2019-05-12 16:26:02 -0500

    add in infowindow method to have google map to defualt to users location

--- TrashCollector/Front End/Index.js
+++ TrashCollector/Front End/Index.js
@@ -1,10 +1,35 @@
-﻿var map;
+﻿var map, infoWindow;
 
 function createMap() {
     var options = {
         center: { lat: 43.038902, lng: -87.906471 },
         zoom: 12
     };
-
+            //start map
     map = new google.maps.Map(document.getElementById('map'), options);
+
+
+    infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (p) {
+            var position = {
+                lat: p.coords.latitude,
+                lng: p.coords.longitude
+            };
+            infoWindow.setPosition(position);
+            infoWindow.setContent('Your location!');
+            infoWindow.open(map);
+        }, function () {
+           handleLocationError('Geolocation service failed', map.center());
+        })
+    } else {
+        handleLocationError('no geolocation available', map.center());
+    }
+}
+
+function handleLocationError(content, position) {
+    infoWindow.setPosition(position);
+    infoWindow.setContent(content);
+    infoWindow.open(map);
 }
