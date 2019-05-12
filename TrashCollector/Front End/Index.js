a290b2a7e9d667c300b408ccef581e606fe7fe14
DejaMeshae <daemeshae@yahoo.com>
2019-05-12 16:01:21 -0500

    Added in google maps API in the index html file

---
+++ TrashCollector/Front End/Index.js
@@ -0,0 +1,10 @@
+﻿var map;
+
+function createMap() {
+    var options = {
+        center: { lat: 43.038902, lng: -87.906471 },
+        zoom: 12
+    };
+
+    map = new google.maps.Map(document.getElementById('map'), options);
+}
